902409a99d3b2044b89e829846fadd8a3f8ac9fd
Keith Liam Manaloto <keithliamm@gmail.com>
2021-12-01 02:01:06 +0800

    Move header to new component

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,19 +1,13 @@
 import CoverSection from './CoverSection';
 import PillarsSection from './PillarsSection';
-import { Footer } from '../../components'
+import { Footer, Header } from '../../components'
 
 import '../../stylesheets/components/footer.css';
 import '../../stylesheets/pages/landing.css';
 
 const LandingPage = () => (
   <div id="landing">
-    <header>
-      <nav id="nav-links">
-        <a href="#">Meet Our Team</a>
-        <a href="#">Get Involved</a>
-        <a href="#">Contact Us</a>
-      </nav>
-    </header>
+    <Header />
     <div className="container">
       <CoverSection />
       <div className="content-container">
