d652754546be85fc3ed0fe0182e1465d5e324dfe
Keith Liam Manaloto <keithliamm@gmail.com>
2021-12-01 02:42:32 +0800

    Update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,29 @@
+import classNames from 'classnames';
+import { useScroll, useDimensions } from 'react-viewport-utils';
+import wingGold from '../assets/images/wing-gold.svg'
+
 import '../stylesheets/components/header.css';
 
-const Header = () => (
-  <header>
-    <nav class="nav-links">
-      <a href="#">Meet Our Team</a>
-      <a href="#">Get Involved</a>
-      <a href="#">Contact Us</a>
-    </nav>
-  </header>
-);
+const Header = () => {
+  const scroll = useScroll();
+  const dimensions = useDimensions();
+
+  const viewportHeight = dimensions.height;
+  const whiteLinks = scroll.y >= viewportHeight * 0.4;
+  const transparent = scroll.y < viewportHeight - 40;
+
+  return (
+    <header className={classNames({ transparent })}>
+      <a className="logo" href="#">
+        Báyung <span>Ángeles<img src={wingGold} alt="wing" /></span>
+      </a>
+      <nav className={classNames('nav-links', { 'white-links': whiteLinks })}>
+        <a href="#">Meet Our Team</a>
+        <a href="#">Get Involved</a>
+        <a href="#">Contact Us</a>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
