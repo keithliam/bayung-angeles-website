f36356c01efb8f4ea02bd466618add6d2852aef6
Keith Liam Manaloto <keithliamm@gmail.com>
2021-11-30 17:57:23 +0800

    Move cover section to new component

---
+++ src/pages/Landing/Landing.js
@@ -0,0 +1,24 @@
+import CoverSection from './CoverSection';
+import { Footer } from '../components'
+
+import '../stylesheets/components/footer.css';
+import '../stylesheets/pages/landing.css';
+
+const LandingPage = () => (
+  <div id="landing">
+    <header>
+      <nav id="nav-links">
+        <a href="#">Meet Our Team</a>
+        <a href="#">Get Involved</a>
+        <a href="#">Contact Us</a>
+      </nav>
+    </header>
+    <div className="container">
+      <CoverSection />
+      <div className="content-container"></div>
+      <Footer />
+    </div>
+  </div>
+);
+
+export default LandingPage;
