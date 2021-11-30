f1afc8fa3d67b40dbd475d8e1ea2866b52019e63
Keith Manaloto <keithliamm@gmail.com>
2021-12-01 04:06:42 +0800

    Add logo and animations to header (#4)
    * Move cover section to new component
    
    * Fix imports
    
    * Add three pillars section
    
    * Use classnames package
    
    * Move header to new component
    
    * Remove duplicate footer css import
    
    * Make photo credits glow on hover
    
    * Change photo credit on-hover transition timing
    
    * Update header
    
    * Recalculate header layout on resize

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,48 @@
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { useScroll, useDimensions } from 'react-viewport-utils';
+import classNames from 'classnames';
+import wingGold from '../assets/images/wing-gold.svg'
+
 import '../stylesheets/components/header.css';
 
-const Header = () => (
-  <header>
-    <nav class="nav-links">
-      <a href="#">Meet Our Team</a>
-      <a href="#">Get Involved</a>
-      <a href="#">Contact Us</a>
-    </nav>
-  </header>
-);
+const Header = () => {
+  const ref = useRef();
+  const scroll = useScroll();
+  const dimensions = useDimensions();
+  
+  const [logoWidth, setLogoWidth] = useState(0);
+  
+  const viewportHeight = dimensions.height;
+  const logoAppearAt = viewportHeight * 0.4
+  const transparentBefore = viewportHeight - 40;
+
+  const logoAppear = scroll.y >= logoAppearAt;
+  const transparent = scroll.y < transparentBefore;
+  const linksOffsetFromLeft = logoAppear ? 0 : logoWidth;
+  
+  const updateLogoWidth = () => setLogoWidth(ref.current.getBoundingClientRect().width);
+
+  useEffect(() => {
+    window.addEventListener('resize', updateLogoWidth);
+    return () => window.removeEventListener('resize', updateLogoWidth);
+  }, [])
+
+  useLayoutEffect(() => {
+    updateLogoWidth();
+  }, [])
+
+  return (
+    <header className={classNames({ transparent, 'logo-appear': logoAppear })}>
+      <a ref={ref} className="logo" href="#">
+        Báyung <span>Ángeles<img src={wingGold} alt="wing" /></span>
+      </a>
+      <nav style={{ transform: `translateX(-${linksOffsetFromLeft}px)`}} className="nav-links">
+        <a href="#">Meet Our Team</a>
+        <a href="#">Get Involved</a>
+        <a href="#">Contact Us</a>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
