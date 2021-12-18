0ee3960a487dd53f9be2bb2b2a1b369657efc045
Keith Manaloto <keithliamm@gmail.com>
2021-12-18 18:24:31 +0800

    Move configurable site data to data directory (#25)
    * Make text with wing icon reusable
    
    * Add initial layout
    
    * Stop subtracting header height from section
    
    * Fix incorrect header spacing
    
    * Speed up card transition
    
    * Fix cards autoplay
    
    * Fix autoplay restarting on scroll
    
    * Update markdown styles
    
    * Divide section parts into components
    
    * FIx incorrect destructuring
    
    * Add missing keys
    
    * Destructure swiper object
    
    * Fix missing member cards
    
    * Move team data to new file
    
    * Remove bulk exports to avoid variable name conflicts
    
    * Move topics data to new file
    
    * Move imagine section data to new file
    
    * Disable right card nav button when at end of list
    
    * Remove unnecessary forwardRef in Header component
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ src/data/pillars.js
@@ -0,0 +1,17 @@
+export const topics = [
+  {
+    title: 'Mission',
+    descriptionLine1: 'Our mission is to lead Angeles City in honesty,',
+    descriptionLine2: 'trust, compassion, and good governance.',
+  },
+  {
+    title: 'Vision',
+    descriptionLine1: 'We see a future where Angeles City is',
+    descriptionLine2: 'the best city in the Philippines.',
+  },
+  {
+    title: 'Core Values',
+    descriptionLine1: 'Truth. Compassion. Innovation.',
+    descriptionLine2: 'Cooperation. Leadership.',
+  },
+];
