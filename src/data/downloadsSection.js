064bf232c0f13af77e34c15a839719fc837ff6bb
Keith Manaloto <keithliamm@gmail.com>
2021-12-18 23:15:32 +0800

    Add the Downloads section (#31)
    * Add the "Get Involved" section
    
    * Add the Downloads section
    
    * Use smaller logo in asset preview

---
+++ src/data/downloadsSection.js
@@ -0,0 +1,68 @@
+import carSticker from '../assets/images/asset-car-sticker.jpg';
+import badgePin from '../assets/images/asset-badge-pin.jpg';
+import baLogo from '../assets/images/asset-ba-logo.png';
+import baIllustrationAnimated from '../assets/images/asset-ba-illus-animated.gif';
+import fbFrame from '../assets/images/asset-fb-frame.png';
+import jeepSign from '../assets/images/asset-jeep-sign.png';
+
+export const previewAssets = [
+  {
+    image: carSticker,
+    name: 'Car Sticker',
+    link: '',
+    style: {
+      top: '17px',
+      height: '217px',
+    },
+  },
+  {
+    image: badgePin,
+    name: 'Badge Pin',
+    link: '',
+    style: {
+      top: '0',
+      right: '59px',
+      width: '171px',
+    },
+  },
+  {
+    image: baIllustrationAnimated,
+    name: 'Báyung Ángeles Illustration',
+    link: '',
+    style: {
+      top: '139px',
+      left: '37px',
+      height: '122px',
+    },
+  },
+  {
+    image: baLogo,
+    name: 'Báyung Ángeles Logo',
+    link: '',
+    style: {
+      top: '89px',
+      left: '145px',
+      height: '137px',
+    },
+  },
+  {
+    image: fbFrame,
+    name: 'Facebook Frame',
+    link: '',
+    style: {
+      top: '120px',
+      right: '0',
+      height: '155px',
+    },
+  },
+  {
+    image: jeepSign,
+    name: 'Jeepney Sign',
+    link: '',
+    style: {
+      bottom: '0',
+      left: '101px',
+      width: '183px',
+    },
+  },
+];
