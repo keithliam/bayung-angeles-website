6dfce63e12a767c15b5acc3a4ebfb5e81840b590
Keith Liam Manaloto <keithliamm@gmail.com>
2021-12-19 11:42:01 +0800

    Reduce imagine section image scale up velocity

--- src/pages/Landing/ImagineSection.js
+++ src/pages/Landing/ImagineSection.js
@@ -7,7 +7,7 @@ import classNames from 'classnames';
 
 import { allPhotos, getTopicAndPhoto } from '../../data/imagine';
 
-const maxScaleUpPercent = 10;
+const maxScaleUpPercent = 5;
 
 const ImagineSection = () => {
   const [ref, bounds] = useMeasure({ scroll: true });
