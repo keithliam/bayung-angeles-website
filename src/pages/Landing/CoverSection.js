26506bc23756e970b67a888503880536abc47df8
Keith Manaloto <keithliamm@gmail.com>
2021-12-29 19:00:02 +0800

    Add functionality to scroll indicator button in Cover section (#47)
    * Add functionality to scroll indicator button in Cover section

--- src/pages/Landing/CoverSection.js
+++ src/pages/Landing/CoverSection.js
@@ -7,34 +7,44 @@ import caratDown from '../../assets/images/carat-down.svg';
 
 import { socialMediaLinks } from '../../data/socialMediaLinks';
 
-const CoverSection = (props, ref) => (
-  <div ref={ref} className="cover-container">
-    <img src={cover} className="ba-cover" alt="cover" />
-    <div className="cover-headline">
-      <h1>
-        Báyung <WingText text="Ángeles" wingPosition="start" />
-      </h1>
-      <img src={coverOverlay} className="ba-cover" alt="cover" />
-      <h4>Be part of the movement</h4>
-      <span>Stay connected. Follow our socials.</span>
-      <div className="social-media-links">
-        {socialMediaLinks.map(link => (
-          <SocialMediaLink key={link.name} className="social-media-link" color="blue" link={link} />
-        ))}
-      </div>
-      <div className="cover-gradient">
-        <button type="button">
-          <img src={caratDown} className="scroll-down-indicator" alt="" />
-        </button>
-        <PhotoCredit
-          className="cover-credit"
-          title="Pisamban Maragul"
-          name="Bruno Tiotuico"
-          link="https://bit.ly/bayung-angeles-portfolio-bruno"
-        />
+const CoverSection = ({ pillarsSectionRef }, ref) => {
+  const handleScrollIndicatorClick = () =>
+    pillarsSectionRef.current.scrollIntoView({ behavior: 'smooth' });
+
+  return (
+    <div ref={ref} className="cover-container">
+      <img src={cover} className="ba-cover" alt="cover" />
+      <div className="cover-headline">
+        <h1>
+          Báyung <WingText text="Ángeles" wingPosition="start" />
+        </h1>
+        <img src={coverOverlay} className="ba-cover" alt="cover" />
+        <h4>Be part of the movement</h4>
+        <span>Stay connected. Follow our socials.</span>
+        <div className="social-media-links">
+          {socialMediaLinks.map(link => (
+            <SocialMediaLink
+              key={link.name}
+              className="social-media-link"
+              color="blue"
+              link={link}
+            />
+          ))}
+        </div>
+        <div className="cover-gradient">
+          <button type="button" onClick={handleScrollIndicatorClick}>
+            <img src={caratDown} className="scroll-down-indicator" alt="Scroll down" />
+          </button>
+          <PhotoCredit
+            className="cover-credit"
+            title="Pisamban Maragul"
+            name="Bruno Tiotuico"
+            link="https://bit.ly/bayung-angeles-portfolio-bruno"
+          />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default forwardRef(CoverSection);
