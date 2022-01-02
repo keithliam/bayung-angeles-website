0b22743fb816380df361473b2625272b01d2cec4
Keith Liam Manaloto <keithliamm@gmail.com>
2022-01-02 18:58:42 +0800

    Use default export for navbar

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export * from './Navbar';
+export { default as Navbar } from './Navbar';
 export { default as Footer } from './Footer';
 export { default as PhotoCredit } from './PhotoCredit';
 export { default as SocialMediaLink } from './SocialMediaLink';
