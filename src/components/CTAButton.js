40c72b41d0b5eaa31daa12f4bf9159c09e92cd92
Keith Liam Manaloto <keithliamm@gmail.com>
2022-02-16 12:46:08 +0800

    Remove target and rel props from CTAButton

--- src/components/CTAButton.js
+++ src/components/CTAButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const CTAButton = ({ className: customClassname, color, text, onClick, href, target, rel }) => {
+const CTAButton = ({ className: customClassname, color, text, onClick, href }) => {
   const className = classNames('cta-button', customClassname, {
     'btn-blue': color === 'blue',
     'btn-gold': color === 'gold',
@@ -9,7 +9,7 @@ const CTAButton = ({ className: customClassname, color, text, onClick, href, tar
 
   if (href) {
     return (
-      <a className={className} href={href} target={target} rel={rel} onClick={onClick}>
+      <a className={className} href={href} target="_blank" rel="noreferrer" onClick={onClick}>
         {text}
       </a>
     );
