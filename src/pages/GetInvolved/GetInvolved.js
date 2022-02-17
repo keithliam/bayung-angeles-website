8841f3991cce7dd44d6643316527df2a8110f504
Keith Liam Manaloto <keithliamm@gmail.com>
2022-02-17 15:23:07 +0800

    Remove unused import

--- src/pages/GetInvolved/GetInvolved.js
+++ src/pages/GetInvolved/GetInvolved.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import StepOneSection from './StepOneSection';
 import StepTwoSection from './StepTwoSection';
