dfb19b675331b622f4cf1bc292c99b816886655f
Keith Manaloto <keithliamm@gmail.com>
2022-02-17 15:27:37 +0800

    Add animations to Get Involved page (#82)
    * Add shaking animation to present emoji in Donation section
    
    * Add transitions to download previews
    
    * Add transitions to step numbers
    
    * Show elements on reload
    
    * Reset animations on scroll up
    
    * Remove unnecessary scroll trigger
    
    * Add missing timeout prop in StepNumber transition
    
    * Rename gift box emoji className in Donation section
    
    * Remove unused import
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/pages/GetInvolved/components/DownloadsPreview.js
+++ src/pages/GetInvolved/components/DownloadsPreview.js
@@ -1,14 +1,40 @@
 import classNames from 'classnames';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
+import { registerScrollResizeEventListeners } from '../../../helpers';
 
-const DownloadsPreview = ({ className, previews }) => (
-  <div className={classNames('downloads-preview', className)}>
-    {previews.map(({ preview, link }) => (
-      <a key={link} className="preview-item" href={link} target="_blank" rel="noreferrer">
-        <img src={preview} alt="Download preview" />
-      </a>
-    ))}
-  </div>
-);
+const DownloadsPreview = ({ className, previews }) => {
+  const ref = useRef();
+  const [previewsAppear, setPreviewsAppear] = useState(false);
+
+  useEffect(() => {
+    const handleScrollResizeEvent = () => {
+      if (ref.current) {
+        const { top } = ref.current.getBoundingClientRect();
+        const newPreviewsAppear = top <= (window.innerHeight / 3) * 2;
+        setPreviewsAppear(newPreviewsAppear);
+      }
+    };
+    handleScrollResizeEvent(); // Trigger immediately to show element if it's already in view
+    const unregisterScrollResizeEventListeners =
+      registerScrollResizeEventListeners(handleScrollResizeEvent);
+
+    return () => {
+      unregisterScrollResizeEventListeners();
+    };
+  }, [previewsAppear]);
+
+  return (
+    <div ref={ref} className={classNames('downloads-preview', className)}>
+      {previews.map(({ preview, link }, index) => (
+        <CSSTransition key={link} in={previewsAppear} timeout={index * 50} classNames="fade-in">
+          <a className="preview-item" href={link} target="_blank" rel="noreferrer">
+            <img src={preview} alt="Download preview" />
+          </a>
+        </CSSTransition>
+      ))}
+    </div>
+  );
+};
 
 export default DownloadsPreview;
