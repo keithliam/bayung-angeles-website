d7ede4f31eb15a74c53ef7f28e1e301ad79d82d9
Keith Liam Manaloto <keithliamm@gmail.com>
2022-02-23 10:53:27 +0800

    Embed small images as base64 strings

---
+++ src/pages/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
