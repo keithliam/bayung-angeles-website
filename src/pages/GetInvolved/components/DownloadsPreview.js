cbdc304615be6584fc4da96830d5d565788a762e
Keith Manaloto <keithliamm@gmail.com>
2022-02-24 15:01:34 +0800

    Fix sticker & poster downloads animation (#95)
    * Implement code-splitting
    
    * Fix typo in loading page
    
    * Fix sticker & poster downloads animation
    
    * Remove newline
    
    * Refactor imports
    
    * Update alt text in error page
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/pages/GetInvolved/components/DownloadsPreview.js
+++ src/pages/GetInvolved/components/DownloadsPreview.js
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
 import React, { useEffect, useRef, useState } from 'react';
 import { CSSTransition } from 'react-transition-group';
+import { prefix } from 'inline-style-prefixer';
 import Img from 'react-cool-img';
 import { registerScrollResizeEventListeners } from '../../../helpers';
 
@@ -31,7 +32,13 @@ const DownloadsPreview = ({ className, previews }) => {
     <div ref={ref} className={classNames('downloads-preview', className)}>
       {previews.map(({ preview, link }, index) => (
         <CSSTransition key={link} in={previewsAppear} timeout={index * 50} classNames="fade-in">
-          <a className="preview-item" href={link} target="_blank" rel="noreferrer">
+          <a
+            className="preview-item"
+            href={link}
+            target="_blank"
+            rel="noreferrer"
+            style={prefix({ transitionDelay: `${index * 50}ms` })}
+          >
             <Img src={preview} placeholder={spinner} alt="Download preview" />
           </a>
         </CSSTransition>
