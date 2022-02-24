d6117d3275516ba941d0f0a1ec748ef5e06d93c1
Keith Manaloto <keithliamm@gmail.com>
2022-02-24 22:59:13 +0800

    Add polyfill for IntersectionObserver (#105)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './stylesheets/index.scss';
 import App from './App';
 
+// IntersectionObserver polyfill
+(async () => {
+  if (!('IntersectionObserver' in window)) await import('intersection-observer');
+})();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
